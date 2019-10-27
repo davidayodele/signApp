da4047d29bd77a132b594c0199582ce1e63d9150
davidayodele <starcanyonschool@gmail.com>
2019-10-27 16:22:11 -0700

    added phone images

---
+++ App/components/component1/Component1.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {AppRegistry, Text, View} from 'react-native';
+
+export default class Component1 extends Component {
+	constructor(props){
+	    super(props);
+	    this.state = {
+	      name: 'David',
+	      showName: false,
+	      message: this.props.message
+	    }
+
+	  }
+	  render(){
+	  	let name = this.state.showName ? this.state.name : 'No name'; // If this.state.name is true return this.state.name, otherwise return 'No name'
+	    return(
+	      <View>
+	      <Text>{this.state.message}</Text>
+	      <Text>{name}</Text>
+	      </View>
+        )
+    };
+}
+
+AppRegistry.registerComponent('Component1', () => MyApp);
